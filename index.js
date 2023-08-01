28b95733bf9c9733961ecfd8f2addbf1df3d3b03
0xSohtan <safinmiah1306@gmail.com>
2023-08-01 04:49:33 +0200

    Game Animation

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
+const playersMoveDiv = document.querySelector('.moves');
+const battleScreen = document.querySelector('.battle');
+
 const score = JSON.parse(localStorage.getItem('score')) || {
     wins: 0,
     niderlagen: 0,
     unentschieden: 0
 }
 
-document.body.addEventListener('keydown', (event) => {
-    if(event.key === 'r') {
-        playGame('stein');
-    } else if(event.key === 's') {
-        playGame('schere');
-    } else if (event.key === 'p') {
-        playGame('papier');
-    }
-})
-
-document.querySelector('.gameScore').innerHTML = `Wins: ${score.wins}, Niderlagen: ${score.niderlagen}, Unentschieden: ${score.unentschieden}`;
-
+// document.body.addEventListener('keydown', function pressKeys(event) {
+//     if(event.key === 'r') {
+//         playGame('Stein');
+//     } else if(event.key === 's') {  
+//         playGame('Schere');
+//     } else if (event.key === 'p') {
+//         playGame('Papier');
+//     }
+// })
 
 function playGame(playersMove) {
+    playersMoveDiv.style.display = 'none';
+    battleScreen.style.display = 'flex';
 
     const computerMove = pickComputerMove();
-
     let result = "";
 
-    if (playersMove === 'schere') {
-        if (computerMove === "stein") {
+
+
+    if (playersMove === 'Schere') {
+        if (computerMove === "Stein") {
             result = "Du hast die Runde verloren.";
-        } else if (computerMove === "papier") {
+        } else if (computerMove === "Papier") { 
             result = "Du hast die Runde gewonnen.";
-        } else if (computerMove === "schere") {
+        } else if (computerMove === "Schere") {
             result = "Unentschieden.";
         }
 
-    } else if (playersMove === 'stein') {
-        if (computerMove === "stein") {
+    } else if (playersMove === 'Stein') {
+        if (computerMove === "Stein") {
             result = "Unentschieden.";
-        } else if (computerMove === "papier") {
+        } else if (computerMove === "Papier") {
             result = "Du hast die Runde verloren.";
-        } else if (computerMove === "schere") {
+        } else if (computerMove === "Schere") {
             result = "Du hast die Runde gewonnen.";
         }
 
-    } else if (playersMove === "papier") {
-        if (computerMove === "stein") {
+    } else if (playersMove === "Papier") {
+        if (computerMove === "Stein") {
             result = "Du hast die Runde gewonnen.";
-        } else if (computerMove === "papier") {
+        } else if (computerMove === "Papier") {
             result = "Unentschieden.";
-        } else if (computerMove === "schere") {
+        } else if (computerMove === "Schere") {
             result = "Du hast die Runde verloren.";
         }
     }
 
+    battleScreen.innerHTML = `<img src="assets/${playersMove}.png"><p>vs.</p><img src="assets/${computerMove}.png">`
+    
     if (result === 'Du hast die Runde gewonnen.') {
         score.wins += 1;
     } else if (result === 'Du hast die Runde verloren.') {
@@ -60,23 +65,25 @@ function playGame(playersMove) {
     }
 
     if (score.wins === 3) {
-        document.querySelector('.gameScore').innerHTML = `Wins: ${score.wins}, Losses: ${score.niderlagen}, Ties: ${score.unentschieden}`;
         document.querySelector('.gameResult').innerHTML = `Du hast das Spiel gewonnen.`;
-        resetScore();
+        endScreen();
         return;
     } else if (score.niderlagen === 3) {
-        document.querySelector('.gameScore').innerHTML = `Wins: ${score.wins}, Losses: ${score.niderlagen}, Ties: ${score.unentschieden}`;
         document.querySelector('.gameResult').innerHTML = `Du hast das Spiel verloren.`;
-        resetScore();
+        endScreen();
         return;
     }
+
+    setTimeout(() => {
+        playersMoveDiv.style.display = 'block';
+        battleScreen.innerHTML = '';
+    }, 5000);
 
     localStorage.setItem('score', JSON.stringify(score));
 
     updateScore();
 
     document.querySelector('.gameResult').innerHTML = `${result}`;
-    document.querySelector('.gameMove').innerHTML = `Du hast ${playersMove} ausgewählt. Dein Gegner hat ${computerMove} ausgewählt.`;
 }
 
 function pickComputerMove() {
@@ -86,26 +93,41 @@ function pickComputerMove() {
     let computerMove = "";
 
     if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = "stein";
+        computerMove = "Stein";
     } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = "papier";
+        computerMove = "Papier";
     } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        computerMove = "schere";
+        computerMove = "Schere";
     }
 
     return computerMove;
-}
+}   
 
 function resetScore() {
     score.wins = 0;
-    score.niderlagen = 0;
+    score.niderlagen = 0;   
     score.unentschieden = 0;
     localStorage.removeItem('score');
+    playersMoveDiv.style.display = 'block';
     updateScore();
 }
 
+function selectScore() {
+    document.querySelector('.scoreWins').innerHTML = `Wins: <strong style="color: green;">${score.wins}</strong>`;
+    document.querySelector('.scoreNiderlage').innerHTML = `Niderlage: <strong style="color: red;">${score.niderlagen}</strong>`;
+    document.querySelector('.scoreUnentschieden').innerHTML = `Unentschieden: <strong style="color: blue;">${score.unentschieden}</strong>`;
+}
+
 function updateScore() {
-    document.querySelector('.gameScore').innerHTML = `Wins: ${score.wins}, Niderlagen: ${score.niderlagen}, Unentschieden: ${score.unentschieden}`;
+    selectScore();
     document.querySelector('.gameResult').innerHTML = ``;
     document.querySelector('.gameMove').innerHTML = ``;
+}
+
+function endScreen() {
+    playersMoveDiv.style.display = 'none';
+    selectScore();
+    setTimeout(() => {
+        battleScreen.style.display = 'none';
+    }, 3000);
 }
